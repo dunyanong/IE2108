5c8a6542d14aadf3cb7a11dac51a6e5bca9c087d
Dun Yan <ong.dy2004@gmail.com>
2024-04-28 00:28:44 +0800

    created selection sort

--- bubblesort.py
+++ bubblesort.py
@@ -1,11 +1,10 @@
-# wall is at the back
 def bubblesort(arr):
     for wall in range(len(arr)):
-        # implement 2 pointers j and j + 1
-        for j in range(len(arr) - wall - 1):
+        # create two pointers
+        for j in range(len(arr) -  wall - 1):
             if (arr[j] > arr[j+1]):
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                arr[j], arr[j+1] = arr[j+1], arr[j]
                 
     return arr
-                
-print(bubblesort([-100, 5, 7, 4, 2]))
+
+print(bubblesort([50, 10, 7, 4, 2]))
--- quicksort.py
+++ quicksort.py
@@ -1,28 +1,24 @@
-# return index of the pivot
-def partition(arr, low, high):
-    # putting pivot at the back
-    pivot = arr[high]
-    i = low - 1
-    
-    for j in range(low, high):
-        if (arr[j] < pivot):
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-            
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
-    
-
 def quicksort(arr, low, high):
     if low < high:
         pivot_index = partition(arr, low, high)
-    
+        
         quicksort(arr, low, pivot_index - 1)
         quicksort(arr, pivot_index + 1, high)
+        
+def partition (arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[j], arr[i] = arr[i], arr[j]
+    
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    
+    return i + 1 # return pivot index
 
 
 # Example usage:
 array_to_sort = [10, 7, 8, 9, 1, 5]
-array_length = len(array_to_sort)
-quicksort(array_to_sort, 0, array_length - 1)
+quicksort(array_to_sort, 0, len(array_to_sort) - 1)
 print("Sorted array:", array_to_sort)
